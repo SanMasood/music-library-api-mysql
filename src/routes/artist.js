90868c39b50e7708a5af4baa1b1e3b0a0891a1f3
SanMasood <sanamasood.giki@gmail.com>
2020-05-16 12:17:32 +0100

    Artist name undefined

--- src/routes/artist.js
+++ src/routes/artist.js
@@ -4,6 +4,6 @@ const artistController = require('../controllers/artist');
 
 const router = express.Router();
 
-//router.route('/artists').post(artistController.createArtist);
+router.route('/artists').post(artistController.create);
 
 module.exports = router;
