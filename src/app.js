110a41f2419eaacd1b345689d1de12fa12ac87c9
SanMasood <sanamasood.giki@gmail.com>
2020-05-16 12:06:34 +0100

    Project initialization & tests failing

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const app = express();
 
 const artistRouter = require('./routes/artist');
 const albumRouter = require('./routes/album');
 
-const app = express();
+const artistController = require('./controllers/artist');
+app.post('/artists', artistController.create);
+
+
 
 app.use(express.json());
 
---
+++ src/routes/artist.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const artistController = require('../controllers/artist');
+
+
+const router = express.Router();
+
+//router.route('/artists').post(artistController.createArtist);
+
+module.exports = router;
